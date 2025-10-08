eb072d739f7745616abd3165906579ce64db9cfa
sajad-ahmadnzhad <ahmadnzhad22@gmail.com>
2025-10-08 21:12:52 +0330

    feat: add perpush in husky and add config module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const logger = new Logger('NestApplication');
+
   await app.listen(process.env.PORT ?? 3000);
+
+  if (process.env.NODE_ENV !== 'production') {
+    logger.log(`server ready on production url: ${await app.getUrl()}`);
+  } else logger.log(`server ready on development url: ${await app.getUrl()}`);
 }
 bootstrap();
